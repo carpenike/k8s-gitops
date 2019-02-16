43c398e8779a5fa91c06ccbfb88cad0b429e1f6e
Ryan Holt <ryan@ryanholt.net>
2019-02-16 21:32:28 +0000

    Add new file

---
+++ manual-steps/create_yamls.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ -z "$DOMAIN" ]]; then
+  echo ".env does not appear to be sourced, sourcing now"
+  . ../.env
+fi
+
+kapply() {
+  envsubst < "$@" | kubectl apply -f -
+}
+
+
+###################
+# traefik-external
+###################
+for i in yamls/traefik-external/*.txt
+do
+  kapply $i
+done
+
+kapply yamls/rook-dashboard-ingress.txt
+
+# loki trial on grafana.net
+kapply yamls/loki.txt
