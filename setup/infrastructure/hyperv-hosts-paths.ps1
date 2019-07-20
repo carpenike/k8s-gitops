77ac3306e44c57365c3fda19e0bfa414ff1e9083
Ryan Holt <ryan@ad.holthome.net>
2019-07-19 22:27:51 -0400

    add infrastructure configs

---
+++ setup/infrastructure/hyperv-hosts-paths.ps1
@@ -0,0 +1,33 @@
+Configuration HyperVHostPaths
+{
+    param
+    (
+        [Parameter(Mandatory=$true, Position=0)]
+        #[ValidateScript({Test-Path $_})]
+        $VirtualHardDiskPath,
+
+        [Parameter(Mandatory=$true, Position=1)]
+        #[ValidateScript({Test-Path $_})]
+        $VirtualMachinePath
+    )
+
+    Import-DscResource -moduleName xHyper-V
+    Node HV02 {
+        xVMHost HyperVHostPaths
+        {
+            IsSingleInstance    = 'Yes'
+            VirtualHardDiskPath = $VirtualHardDiskPath
+            VirtualMachinePath  = $VirtualMachinePath
+        }
+    }
+    Node HV03 {
+        xVMHost HyperVHostPaths
+        {
+            IsSingleInstance    = 'Yes'
+            VirtualHardDiskPath = $VirtualHardDiskPath
+            VirtualMachinePath  = $VirtualMachinePath
+        }
+    }
+
+}
+HyperVHostPaths -VirtualHardDiskPath "C:\ClusterStorage\" -VirtualMachinePath "C:\ClusterStorage\"
