700a9f3c693999e70c1d77a658254cc48d65dcd3
Ryan Holt <ryan@ryanholt.net>
2020-01-20 11:10:32 -0500

    fix stuff

--- setup/bootstrap-cluster.sh
+++ setup/bootstrap-cluster.sh
@@ -3,8 +3,8 @@
 # nodes
 K3S_MASTER="k3s-0"
 K3S_WORKERS_AMD64="k3s-1 k3s-2"
-K3S_WORKERS_ODROID="k3s-3"
-K3S_WORKERS_RPI=""
+K3S_WORKERS_RPI_ARM64=""
+K3S_WORKERS_ODROID_ARM64="k3s-3"
 K3S_VERSION="v1.17.0+k3s.1"
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
@@ -40,27 +40,20 @@ ks3amd64WorkerNodes() {
   done
 }
 
-ks3OdroidWorkerNodes() {
+ks3arm64WorkerNodes() {
   NODE_TOKEN=$(ssh -o "StrictHostKeyChecking=no" ubuntu@"$K3S_MASTER" "sudo cat /var/lib/rancher/k3s/server/node-token")
-  for node in $K3S_WORKERS_ODROID; do
-    message "joining amd64 $node to $K3S_MASTER"
+  for node in $K3S_WORKERS_RPI_ARM64; do
+    message "joining pi4 $node to $K3S_MASTER"
+    EXTRA_ARGS=""
+    ssh -o "StrictHostKeyChecking=no" ubuntu@"$node" "curl -sfL https://get.k3s.io | K3S_URL=https://k3s-0:6443 K3S_TOKEN=$NODE_TOKEN INSTALL_K3S_VERSION='$K3S_VERSION' sh -s - --node-taint arm=true:NoExecute $EXTRA_ARGS"
+  done
+  for node in $K3S_WORKERS_ODROID_ARM64; do
+    message "joining ODROID $node to $K3S_MASTER"
     EXTRA_ARGS="--docker"
     if [ "$node" == "k3s-3" ]; then
       EXTRA_ARGS="$EXTRA_ARGS --node-label tpu=google-coral"
     fi
-    ssh -o "StrictHostKeyChecking=no" ubuntu@"$node" "curl -sfL https://get.k3s.io | K3S_URL=https://k3s-0:6443 K3S_TOKEN=$NODE_TOKEN INSTALL_K3S_VERSION='$K3S_VERSION' sh -s - $EXTRA_ARGS"
-  done
-}
-
-ks3armWorkerNodes() {
-  NODE_TOKEN=$(ssh -o "StrictHostKeyChecking=no" ubuntu@"$K3S_MASTER" "sudo cat /var/lib/rancher/k3s/server/node-token")
-  for node in $K3S_WORKERS_RPI; do
-    message "joining pi4 $node to $K3S_MASTER"
-    EXTRA_ARGS=""
-    if [ "$node" == "pi4-c" ]; then
-      EXTRA_ARGS="--node-label usb=alarmdecoder"
-    fi
-    ssh -o "StrictHostKeyChecking=no" pi@"$node" "curl -sfL https://get.k3s.io | K3S_URL=https://k3s-0:6443 K3S_TOKEN=$NODE_TOKEN INSTALL_K3S_VERSION='$K3S_VERSION' sh -s - --node-taint arm=true:NoExecute --data-dir /mnt/usb/var/lib/rancher $EXTRA_ARGS"
+    ssh -o "StrictHostKeyChecking=no" ubuntu@"$node" "curl -sfL https://get.k3s.io | K3S_URL=https://k3s-0:6443 K3S_TOKEN=$NODE_TOKEN INSTALL_K3S_VERSION='$K3S_VERSION' sh -s - --node-taint arm=true:NoExecute $EXTRA_ARGS"
   done
 }
 
@@ -97,14 +90,13 @@ installFlux() {
   # grab output the key
   FLUX_KEY=$(kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2)
 
-  #message "adding the key to github automatically"
-  #"$REPO_ROOT"/setup/add-repo-key.sh "$FLUX_KEY"
+  # message "adding the key to github automatically"
+  # "$REPO_ROOT"/setup/add-repo-key.sh "$FLUX_KEY"
 }
 
 k3sMasterNode
 ks3amd64WorkerNodes
-ks3OdroidWorkerNodes
-# ks3armWorkerNodes
+ks3arm64WorkerNodes
 
 export KUBECONFIG="$REPO_ROOT/setup/kubeconfig"
 installHelm
