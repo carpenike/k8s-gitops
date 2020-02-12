7fbaab7eea312d0a4b346120e35790d5e53ed0dc
Ryan Holt <ryan@ryanholt.net>
2020-02-12 16:28:47 +0000

    create restore helper script for rbds

---
+++ setup/restore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#RBDTARGETS=("sonarr-config" "nzbhydra2-config" "radarr-config")
+RBDTARGETS=("radarr-config")
+PVTARGETS=("nzbget-config")
+FOLDER="2019.11.20"
+TOOLS=`kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}'`
+
+echo "Mounting NFS Storage Location where Backups are"
+kubectl -n rook-ceph exec -it "$TOOLS" -- sh -c "mkdir -p /mnt/nfs && mount -t nfs storage:/mnt/tank/share /mnt/nfs || /bin/true"
+
+for i in "${RBDTARGETS[@]}"
+do
+    echo "Processing $i..."
+    PV=csi-vol-`kubectl get pvc/$i -o=jsonpath='{.spec.volumeName}' | xargs -I {} kubectl get pv {} -o=jsonpath='{.spec.csi.volumeHandle}' | cut -d '-' --fields=6-10`
+    echo "Ceph RBD is $PV..."
+    kubectl -n rook-ceph exec -it "$TOOLS" -- sh -c "DEV=\$(rbd map replicapool/$PV) && mkdir -p /tmp/rbd && mount \$DEV /tmp/rbd && rm -rf /tmp/rbd/* && cp -rvapf /mnt/nfs/$FOLDER/$i/* /tmp/rbd/ && cd /tmp; umount /tmp/rbd && rbd unmap \$DEV"
+done
