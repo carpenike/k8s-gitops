7c811ad9cef9f294db09e136dde287be6ceb501d
Ryan Holt <ryan@ryanholt.net>
2020-07-24 15:11:21 -0400

    enable oauth2-proxy
    Signed-off-by: Ryan Holt <ryan@ryanholt.net>

--- secrets/secrets.sh
+++ secrets/secrets.sh
@@ -91,6 +91,7 @@ kseal "${REPO_ROOT}/cluster/monitoring/uptimerobot/uptimerobot-helm-values.txt"
 kseal "${REPO_ROOT}/cluster/monitoring/thanos/thanos-helm-values.txt"
 kseal "${REPO_ROOT}/cluster/kube-system/keycloak/keycloak-helm-values.txt"
 kseal "${REPO_ROOT}/cluster/kube-system/external-dns/external-dns-helm-values.txt"
+kseal "${REPO_ROOT}/cluster/kube-system/oauth2-proxy/oauth2-proxy-helm-values.txt"
 
 #
 # Generic Secrets
