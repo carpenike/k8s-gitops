1e89028adadb1129d7b143bac98f8f31cfb337ca
Ryan Holt <ryan@ryanholt.net>
2021-08-26 23:40:56 -0400

    exclude ownerReferences as well

--- cluster/core/rook-ceph/reflector/configmap-reflector.sh
+++ cluster/core/rook-ceph/reflector/configmap-reflector.sh
@@ -9,11 +9,11 @@ namespace_source="rook-ceph"
 # space delimited namespace where to reflect the configmaps to
 namespace_destination="backup-system"
 for configmap in ${configmaps}; do
-    configmap_source_content="$(kubectl get configmap "${configmap}" -n "${namespace_source}" -o json | jq 'del(.metadata.managedFields, .metadata.creationTimestamp, .metadata.resourceVersion, .metadata.uid)')"
+    configmap_source_content="$(kubectl get configmap "${configmap}" -n "${namespace_source}" -o json | jq 'del(.metadata.managedFields, .metadata.creationTimestamp, .metadata.resourceVersion, .metadata.uid, .metadata.ownerReferences)')"
     configmap_source_checksum="$(echo "${configmap_source_content}" | jq 'del(.metadata.namespace)' | md5sum | awk '{ print $1 }')"
     for namespace in ${namespace_destination}; do
         if kubectl get configmap "${configmap}" -n "${namespace}" >/dev/null 2>&1; then
-            configmap_dest_content="$(kubectl get configmap "${configmap}" -n "${namespace}" -o json | jq 'del(.metadata.managedFields, .metadata.creationTimestamp, .metadata.resourceVersion, .metadata.uid)')"
+            configmap_dest_content="$(kubectl get configmap "${configmap}" -n "${namespace}" -o json | jq 'del(.metadata.managedFields, .metadata.creationTimestamp, .metadata.resourceVersion, .metadata.uid, .metadata.ownerReferences)')"
             configmap_dest_checksum="$(echo "${configmap_dest_content}" | jq 'del(.metadata.namespace)' | md5sum | awk '{ print $1 }')"
             if [ "${configmap_source_checksum}" != "${configmap_dest_checksum}" ]; then
                 echo "${configmap_source_content}" |
