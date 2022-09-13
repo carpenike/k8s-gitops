a41464b71c38e92d3ea380f078f309028cfd74b1
Ryan Holt <ryan@ryanholt.net>
2022-09-13 16:00:43 -0400

    set correct interface for dyndns

--- infrastructure/vyos/config/service-dns-dynamic.tf
+++ infrastructure/vyos/config/service-dns-dynamic.tf
@@ -4,12 +4,12 @@ resource "vyos_config_block_tree" "service-dns-dynamic" {
   configs = merge(
     merge([
       for domain in ["family", "personal", "ingress"] : {
-        "interface eth0 service ${domain} host-name" = "gateway.${var.domains[domain]}"
-        "interface eth0 service ${domain} server"    = "api.cloudflare.com/client/v4"
-        "interface eth0 service ${domain} protocol"  = "cloudflare"
-        "interface eth0 service ${domain} zone"      = "${var.domains[domain]}"
-        "interface eth0 service ${domain} login"     = "${var.secrets.cloudflare.login}"
-        "interface eth0 service ${domain} password"  = "${var.secrets.cloudflare.key}"
+        "interface bond0.4000 service ${domain} host-name" = "ipv4.${var.domains[domain]}"
+        "interface bond0.4000 service ${domain} server"    = "api.cloudflare.com/client/v4"
+        "interface bond0.4000 service ${domain} protocol"  = "cloudflare"
+        "interface bond0.4000 service ${domain} zone"      = "${var.domains[domain]}"
+        "interface bond0.4000 service ${domain} login"     = "${var.secrets.cloudflare.login}"
+        "interface bond0.4000 service ${domain} password"  = "${var.secrets.cloudflare.key}"
       }
     ]...),
   )
