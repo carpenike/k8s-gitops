c15ee20bb1dc5c316b510b36d92f2e0ee40b5fe5
Ryan Holt <ryan@ryanholt.net>
2023-08-15 09:35:28 -0400

    change the secret path

--- kubernetes/cluster-0/apps/media/qbittorrent/app/configs/config.js
+++ kubernetes/cluster-0/apps/media/qbittorrent/app/configs/config.js
@@ -8,12 +8,12 @@ module.exports = {
   qbittorrentUrl: "http://localhost:80",
 
   torznab: [
-    "http://prowlarr.media.svc.cluster.local:80/13/api?apikey={{ api_key }}", // tl
-    "http://prowlarr.media.svc.cluster.local:80/7/api?apikey={{ api_key }}", // fl
-    "http://prowlarr.media.svc.cluster.local:80/6/api?apikey={{ api_key }}", // st
-    "http://prowlarr.media.svc.cluster.local:80/8/api?apikey={{ api_key }}", // blu
-    "http://prowlarr.media.svc.cluster.local:80/3/api?apikey={{ api_key }}", // btn
-    "http://prowlarr.media.svc.cluster.local:80/5/api?apikey={{ api_key }}", // or
+    "http://prowlarr.media.svc.cluster.local:80/13/api?apikey={{ .api_key }}", // tl
+    "http://prowlarr.media.svc.cluster.local:80/7/api?apikey={{ .api_key }}", // fl
+    "http://prowlarr.media.svc.cluster.local:80/6/api?apikey={{ .api_key }}", // st
+    "http://prowlarr.media.svc.cluster.local:80/8/api?apikey={{ .api_key }}", // blu
+    "http://prowlarr.media.svc.cluster.local:80/3/api?apikey={{ .api_key }}", // btn
+    "http://prowlarr.media.svc.cluster.local:80/5/api?apikey={{ .api_key }}", // or
   ],
 
   action: "inject",
