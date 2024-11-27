1dcf1eebe36a010211a5b57e6aa078c2975dc783
Ryan Holt <ryan@ryanholt.net>
2024-11-27 10:11:50 -0500

    deprecate all the terraform stuff

--- infrastructure/vyos/modules/config/scripts.tf
+++
@@ -1,8 +0,0 @@
-resource "remote_file" "scripts-precommit-add_container_images" {
-  provider    = remote
-  path        = "/config/scripts/commit/pre-hooks.d/add-container-images"
-  content     = file(pathexpand("${path.root}/scripts/commit/pre-hooks.d/add-container-images"))
-  permissions = "0775"
-  owner       = "0"
-  group       = "104"
-}
