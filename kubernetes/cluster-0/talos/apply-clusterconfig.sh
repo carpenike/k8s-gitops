d1fefda47e63bad596ce9fe140d54a63c224c86a
Ryan Holt <ryan@ryanholt.net>
2025-04-01 21:32:45 -0400

    deprecate templates folder

--- kubernetes/cluster-0/talos/apply-clusterconfig.sh
+++ kubernetes/cluster-0/talos/apply-clusterconfig.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n cp-0.holthome.net -f ./clusterconfig/cluster-0-cp-0.holthome.net.yaml
-talosctl apply-config -n node-0.holthome.net -f ./clusterconfig/cluster-0-node-0.holthome.net.yaml
-talosctl apply-config -n node-1.holthome.net -f ./clusterconfig/cluster-0-node-1.holthome.net.yaml
-talosctl apply-config -n node-2.holthome.net -f ./clusterconfig/cluster-0-node-2.holthome.net.yaml
-talosctl apply-config -n node-3.holthome.net -f ./clusterconfig/cluster-0-node-3.holthome.net.yaml
+/opt/homebrew/bin/talosctl apply-config -n cp-0.holthome.net -f ./clusterconfig/cluster-0-cp-0.holthome.net.yaml
+/opt/homebrew/bin/talosctl apply-config -n node-0.holthome.net -f ./clusterconfig/cluster-0-node-0.holthome.net.yaml
+/opt/homebrew/bin/talosctl apply-config -n node-1.holthome.net -f ./clusterconfig/cluster-0-node-1.holthome.net.yaml
+/opt/homebrew/bin/talosctl apply-config -n node-2.holthome.net -f ./clusterconfig/cluster-0-node-2.holthome.net.yaml
+/opt/homebrew/bin/talosctl apply-config -n node-3.holthome.net -f ./clusterconfig/cluster-0-node-3.holthome.net.yaml
